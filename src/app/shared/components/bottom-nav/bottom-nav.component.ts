17b630dd5ab79d19f40e2af5c1806661aa2cdd5b
bastrodriguezr <bast.rodriguezr@gmail.com>
2024-10-20 23:00:27 -0300

    Actualización de Diseño y Conversión de Header y Footer a componente

---
+++ src/app/shared/components/bottom-nav/bottom-nav.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { Router } from '@angular/router'; // Asegúrate de importar Router
+
+@Component({
+  selector: 'app-bottom-nav',
+  templateUrl: './bottom-nav.component.html',
+  styleUrls: ['./bottom-nav.component.scss'],
+})
+export class BottomNavComponent {
+  firebaseSvc = inject(FirebaseService);
+
+  constructor(private navCtrl: NavController, private router: Router) {} // Inyectar Router
+
+  Profile() {
+    this.router.navigate(['/profile']);  // Navegar a la página de perfil
+  }
+
+  home(){
+    this.router.navigate(['main/home']);
+  }
+
+  signOut() {
+    this.firebaseSvc.signOut();
+  }
+
+}
